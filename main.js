b108f298a6949071c08f09c9dcd3bab50cee907a
Jacob Hill <jdhill44@tntech.edu>
2020-12-01 20:44:54 -0600

    Removed top button.

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 document.addEventListener("scroll", () => {
   var header = document.querySelector(".main-header");
-  var up = document.querySelector(".up");
   if (window.scrollY > 0) {
     header.classList.add("sticky");
-    up.classList.add("visible");
   } else {
     header.classList.remove("sticky");
-    up.classList.remove("visible");
   }
 });
